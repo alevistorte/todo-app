c26f09f2e8537629e9a81e6b76b84931d635c9d8
alevistorte <avsdevsubs@gmail.com>
2022-02-26 00:13:01 -0500

    Adding readme

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { TodoApp } from "./components/08-useReducer/TodoApp";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
