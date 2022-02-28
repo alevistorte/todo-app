d1bc68216ef204a0762be59d79c9e4889ef7da65
alevistorte <avsdevsubs@gmail.com>
2022-02-28 13:58:01 -0500

    UseContext practice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { TodoApp } from "./components/08-useReducer/TodoApp";
+import { MainApp } from "./components/09-useContext/MainApp";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<TodoApp />, document.getElementById("root"));
+ReactDOM.render(<MainApp />, document.getElementById("root"));
